714f2a60e9963534c8bff12549d255a5b8304776
alexeevich <alexeevich@Trofimov>
2022-05-14 11:56:42 +0500

    Трофимов. Создал классы, содержащие формулы для вычислений на основе абстрактных классов.

--- src/main/java/project/Event.java
+++ src/main/java/project/Event.java
@@ -1,5 +1,15 @@
 package project;
 
-public class Event {
-
+public final class Event extends calculate{
+	
+	private final Double event;
+	
+	public Event(Double event) {
+		this.event = event;
+}
+	
+	@Override
+	public Double calculation(){
+		return event;
+	}
 }
